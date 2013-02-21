592ea044d6d2e1b2a338d7311b5287d0c2f51243
Sindre Sorhus <sindresorhus@gmail.com>
2013-02-21 21:09:23 +0100

    Remove trailing whitespace

--- tasks/bower-hooks.js
+++ tasks/bower-hooks.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = function (grunt) {
 	var _ = require('lodash');
-	
 	var requirejs = require('requirejs/bin/r.js');
 
 	grunt.registerMultiTask('bower', 'Wire-up Bower components in RJS config', function () {
 		var cb = this.async();
 		var filePath = this.data.rjsConfig;
 		var file = grunt.file.read(filePath);
+
 		require('bower').commands.list({paths: true})
 			.on('data', function (data) {
 				var rjsConfig;
@@ -17,18 +17,17 @@ module.exports = function (grunt) {
 					data = _.forOwn(data, function (val, key, obj) {
 						obj[key] = grunt.file.isDir(val) ? val : val.replace(/\.js$/, '');
 					});
-					
-					requirejs.tools.useLib(function(require) {
-						rjsConfig = require('transform').modifyConfig(file, function(config) {
+
+					requirejs.tools.useLib(function (require) {
+						rjsConfig = require('transform').modifyConfig(file, function (config) {
 							_.extend(config.paths, data);
 							return config;
 						});
-						
+
 						grunt.file.write(filePath, rjsConfig);
 						grunt.log.writeln('Updated RequireJS config with installed Bower components'.green);
 						cb();
-					});					
-					
+					});
 				}
 			})
 			.on('error', function (err) {
