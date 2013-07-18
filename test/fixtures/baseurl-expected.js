b2a0b46f021560774636fe3f3956b3d84876ec8d
Sindre Sorhus <sindresorhus@gmail.com>
2013-07-19 00:44:18 +0200

    Fix tests for latest Bower

--- test/fixtures/baseurl-expected.js
+++ test/fixtures/baseurl-expected.js
@@ -5,13 +5,13 @@ require.config({
   paths: {
     hm: 'scripts/vendor/hm',
     esprima: 'scripts/vendor/esprima',
-    anima: 'components/anima/anima.min',
-    jquery: 'components/jquery/jquery',
-    'jquery-ui-touch-punch-amd': 'components/jquery-ui-touch-punch-amd/jquery.ui.touch-punch',
-    respond: 'components/respond/respond.src',
-    typeahead: 'components/typeahead.js/dist/typeahead',
-    highcharts: 'components/highstock/js/highcharts.src',
-    highstock: 'components/highstock/js/highstock.src',
-    'highcharts-more': 'components/highstock/js/highcharts-more.src'
+    anima: 'bower_components/anima/anima.min',
+    jquery: 'bower_components/jquery/jquery',
+    'jquery-ui-touch-punch-amd': 'bower_components/jquery-ui-touch-punch-amd/jquery.ui.touch-punch',
+    respond: 'bower_components/respond/respond.src',
+    typeahead: 'bower_components/typeahead.js/dist/typeahead',
+    highcharts: 'bower_components/highstock/js/highcharts.src',
+    highstock: 'bower_components/highstock/js/highstock.src',
+    'highcharts-more': 'bower_components/highstock/js/highcharts-more.src'
   }
 });
