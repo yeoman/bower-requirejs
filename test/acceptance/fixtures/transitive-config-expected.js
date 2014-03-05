1a911ed338601bda53b55f13f7208773a3681d0d
Kevin Martensson <kevinmartensson@gmail.com>
2014-03-05 08:45:34 +0100

    Fix tests

--- test/acceptance/fixtures/transitive-config-expected.js
+++ test/acceptance/fixtures/transitive-config-expected.js
@@ -10,14 +10,14 @@ var require = {
     konamicode: '../bower_components/konamicode.js/build/konamicode.min',
     json2: '../bower_components/json2/json2',
     'jquery-ui-touch-punch-amd': '../bower_components/jquery-ui-touch-punch-amd/jquery.ui.touch-punch',
-    jquery: '../bower_components/jquery/jquery',
     handlebars: '../bower_components/handlebars/handlebars',
     'handlebars.runtime': '../bower_components/handlebars/handlebars.runtime',
+    underscore: '../bower_components/underscore/underscore',
+    jquery: '../bower_components/jquery/jquery',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
-    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.marionette': '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
     'backbone-amd': '../bower_components/backbone-amd/backbone',
-    underscore: '../bower_components/underscore/underscore',
     backbone: '../bower_components/backbone/backbone',
     anima: '../bower_components/anima/anima'
   }
